26651bec32f438f696492f46c6c282aa7abdd5f9
Kris <kpelt@softserveinc.com>
2023-12-30 20:14:21 +0000

    day 5 optimization

--- 2023/day05/main.go
+++ 2023/day05/main.go
@@ -12,26 +12,110 @@ import (
 	"github.com/Kris-Pelteshki/aoc_2023/util"
 	"github.com/Kris-Pelteshki/aoc_2023/util/collections"
 	"github.com/Kris-Pelteshki/aoc_2023/util/maths"
+	"github.com/emirpasic/gods/stacks/arraystack"
 )
 
-type mapRangeType struct {
-	sourceStart      uint
-	sourceEnd        uint
-	length           uint
-	destinationStart uint
+type Interval struct {
+	start uint
+	end   uint
+}
+
+type MapRange struct {
+	source      Interval
+	destination Interval
+}
+
+func (mr *MapRange) contains(value uint) bool {
+	return value >= mr.source.start && value <= mr.source.end
+}
+
+func (mr *MapRange) getDestinationValue(value uint) uint {
+	return mr.destination.start + (value - mr.source.start)
+}
+
+func newMapRange(destinationStart, sourceStart, len uint) MapRange {
+	return MapRange{
+		source: Interval{
+			start: sourceStart,
+			end:   sourceStart + len - 1,
+		},
+		destination: Interval{
+			start: destinationStart,
+			end:   destinationStart + len - 1,
+		},
+	}
 }
 
 type ConversionMap struct {
-	from   string
-	to     string
-	ranges []mapRangeType
+	ranges []MapRange
 }
 
-func newMapRange(destinationStart, start, len uint) mapRangeType {
-	return mapRangeType{start, start + len, len, destinationStart}
+func (cm *ConversionMap) mapValue(value uint) uint {
+	for _, mapRange := range cm.ranges {
+		if mapRange.contains(value) {
+			return mapRange.getDestinationValue(value)
+		}
+	}
+	return value
 }
 
-type SeedStrategy = func(string) []uint
+func (cm *ConversionMap) splitIntervals(intervals []Interval) []Interval {
+	var result []Interval
+	stack := arraystack.New()
+
+	for _, interval := range intervals {
+		stack.Push(interval)
+	}
+
+OuterLoop:
+	for !stack.Empty() {
+		val, _ := stack.Pop()
+		interval := val.(Interval)
+
+		for _, mapRange := range cm.ranges {
+			min := maths.Max(interval.start, mapRange.source.start)
+			max := maths.Min(interval.end, mapRange.source.end)
+
+			if min <= max {
+				result = append(result, Interval{min, max})
+
+				if min > interval.start {
+					stack.Push(Interval{interval.start, min - 1})
+				}
+				if max < interval.end {
+					stack.Push(Interval{max + 1, interval.end})
+				}
+
+				continue OuterLoop
+			}
+		}
+		result = append(result, interval)
+	}
+
+	return result
+}
+
+func (cm *ConversionMap) mapIntervals(intervals []Interval) []Interval {
+	var result []Interval
+
+	for _, interval := range intervals {
+		inRange := false
+		for _, mapRange := range cm.ranges {
+			if mapRange.contains(interval.start) {
+				inRange = true
+				result = append(result, Interval{
+					mapRange.getDestinationValue(interval.start),
+					mapRange.getDestinationValue(interval.end),
+				})
+			}
+		}
+
+		if !inRange {
+			result = append(result, interval)
+		}
+	}
+	return result
+}
 
 //go:embed input.txt
 var input string
@@ -64,16 +148,6 @@ func main() {
 	fmt.Println("Time:", time.Since(start))
 }
 
-func mapValue(value uint, conversionMap *ConversionMap) uint {
-	for _, mapRange := range conversionMap.ranges {
-		if value >= mapRange.sourceStart && value < mapRange.sourceEnd {
-			return mapRange.destinationStart + (value - mapRange.sourceStart)
-		}
-	}
-
-	return value
-}
-
 func part1(input string) uint {
 	seeds, maps := parseInput(input)
 	lowestLocations := []uint{}
@@ -81,7 +155,7 @@ func part1(input string) uint {
 	for _, seed := range seeds {
 		ans := seed
 		for _, convMap := range maps {
-			ans = mapValue(ans, convMap)
+			ans = convMap.mapValue(ans)
 		}
 		lowestLocations = append(lowestLocations, ans)
 	}
@@ -89,28 +163,24 @@ func part1(input string) uint {
 	return maths.Min(lowestLocations...)
 }
 
-/*
-* Brute forcing the solution because I'm too tired to reword part 1 to work for part 2.
-* A better way would be to perform a reverse search from the lowest location to the seed... but that's for another time
- */
 func part2(input string) uint {
 	seeds, maps := parseInput(input)
-	minLocation := seeds[0]
-
 	seedPairs := collections.Chunks(seeds, 2)
+	minLocation := seeds[0]
 
 	for _, seedPair := range seedPairs {
 		start := seedPair[0]
-		end := start + seedPair[1]
+		end := start + seedPair[1] - 1
+		intervals := []Interval{{start, end}}
 
-		for i := start; i < end; i++ {
-			ans := i
-			for _, convMap := range maps {
-				ans = mapValue(ans, convMap)
-			}
+		for _, convMap := range maps {
+			intervals = convMap.splitIntervals(intervals)
+			intervals = convMap.mapIntervals(intervals)
+		}
 
-			if ans < minLocation {
-				minLocation = ans
+		for _, interval := range intervals {
+			if interval.start < minLocation {
+				minLocation = interval.start
 			}
 		}
 	}
@@ -119,17 +189,9 @@ func part2(input string) uint {
 }
 
 func buildMapRange(input string) *ConversionMap {
-	ans := ConversionMap{}
+	conversionMap := ConversionMap{}
 
-	name, data, _ := strings.Cut(input, " map:\n")
-
-	from, to, isNameOk := strings.Cut(name, "-to-")
-	if !isNameOk {
-		panic("invalid name")
-	}
-
-	ans.from = from
-	ans.to = to
+	_, data, _ := strings.Cut(input, " map:\n")
 
 	for _, line := range strings.Split(data, "\n") {
 		nums := strings.Fields(line)
@@ -138,26 +200,25 @@ func buildMapRange(input string) *ConversionMap {
 			uint(cast.ToInt(nums[1])),
 			uint(cast.ToInt(nums[2])),
 		)
-		ans.ranges = append(ans.ranges, mapRange)
+		conversionMap.ranges = append(conversionMap.ranges, mapRange)
 	}
 
-	sort.Slice(ans.ranges, func(i, j int) bool {
-		return ans.ranges[i].destinationStart < ans.ranges[j].destinationStart
+	sort.Slice(conversionMap.ranges, func(i, j int) bool {
+		return conversionMap.ranges[i].destination.start < conversionMap.ranges[j].destination.start
 	})
 
-	return &ans
+	return &conversionMap
 }
 
 func parseInput(input string) (seeds []uint, conversionMaps []*ConversionMap) {
 	seperator := "\n\n"
 
 	seedLine, dataLine, _ := strings.Cut(input, seperator)
-	seedStrData := strings.Split(seedLine, ": ")
+	seedStrings := strings.Fields(seedLine)[1:]
 
-	for _, seedStr := range strings.Fields(seedStrData[1]) {
+	for _, seedStr := range seedStrings {
 		seeds = append(seeds, uint(cast.ToInt(seedStr)))
 	}
-
 	for _, mapStr := range strings.Split(dataLine, seperator) {
 		conversionMaps = append(conversionMaps, buildMapRange(mapStr))
 	}
